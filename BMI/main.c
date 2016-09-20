a93602523bb4905a5a69f2e4a04833af238ab185
ChrisSalinasSoto <SSSillyMac@macbook-pro-van-christofer.local>
2016-09-20 14:26:06 +0200

    Initial Commit

---
+++ BMI/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  BMI
+//
+//  Created by Christofer Salinas Soto on 20-09-16.
+//  Copyright © 2016 Christofer Salinas Soto. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
