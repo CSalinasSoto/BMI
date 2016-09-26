5359350a06de4b9643b67851a5d7bfee663e7912
ChrisSalinasSoto <SSSillyMac@MacBook-Pro-van-Christofer.local>
2016-09-26 17:25:49 +0200

    bmi bereken

--- BMI/main.c
+++ BMI/main.c
@@ -1,15 +1,46 @@
-//
-//  main.c
-//  BMI
-//
-//  Created by Christofer Salinas Soto on 20-09-16.
-//  Copyright © 2016 Christofer Salinas Soto. All rights reserved.
-//
-
 #include <stdio.h>
+float lengte;
+float gewicht;
+float bmi;
+float start;
+char keuze;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+void bereken(){
+    printf("Enter your height: ");
+    scanf(" %f", &lengte);
+    
+    printf("\nEnter your weight: ");
+    scanf(" %f", &gewicht);
+    
+    bmi = (gewicht) / (lengte * lengte);
+    
+    printf("\nJe BMI is: %.0f", bmi);
+    
+    if(bmi < 18.5){
+        printf("\nJe hebt ondergewicht");
+    } else if(bmi > 18.5 && bmi < 24.9){
+        printf("\nJe hebt een normaal gewicht");
+    }else if(bmi > 25 && bmi < 29.9){
+        printf("\nJe hebt overgewicht, je mag niet dikker worden");
+    } else if (bmi > 30 && bmi < 39.9){
+        printf("\nJe hebt obesitas");
+    } else if (bmi > 40){
+        printf("\nJe gaat waarschijnlijk zo dood, dus geniet nog even van je leven.");
+    }
+}
+int main()
+{
+    bereken();
+    while(1 == 1){
+        scanf("%c", &keuze);
+        printf("\nwilt u opnieuw uw BMI berekenen? (Y/N)\n");
+        scanf("%c", &keuze);
+        if(keuze == 'y' || keuze == 'Y'){
+            bereken();
+        } else if (keuze == 'n' || keuze == 'N'){
+            return 0;
+        } else {
+            printf("Oncorrect, probeer het opnieuw\n");
+        }
+    }
 }
